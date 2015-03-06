bdca8190f1679738c2961c959bcd0b2f34a08fe5
Yannick Pflanzer <yp1995@outlook.com>
2015-03-06 10:17:15 +0100

    * Cleaned up assembler code

--- tests/os/main.asm
+++ tests/os/main.asm
@@ -19,22 +19,6 @@ start:
 	nop
 	j start
 
-	# 6 * 6 = 36
-	# addi $s0, $0, 6 # $s0 = 6
-	# addi $s1, $0, 3 # $s0 = 6 
-
-	# addi $t0, $0, 0
-	# addi $t1, $0, 6
-
-# loop:
-	# beq $t0, $s1, end
-
-	# addi $s0, $s0, 6 	
-	# addi $t0, $t0, 1
-
-	# addi $t3, $0, 0xDE 
-	# j loop
-
 .data
 string1: .asciiz "Hello World!\n"
 
